3be58d8ce204ce25a6f68000551fbdd2e7f82c39
nikhil <nperi104@uottawa.ca>
2017-06-14 22:15:22 -0400

    110001010101001001

--- app/controllers/broker_controller.rb
+++ app/controllers/broker_controller.rb
@@ -1,7 +1,38 @@
 class BrokerController < ApplicationController
-  before_action :load_broker
+  before_action :load_broker, only: :show
+
+  def new
+    @broker = Broker.new
+  end
+
+  def create
+    @broker = Broker.new(signup_params)
+
+    if @broker.save
+        session[:broker_id] = @broker.id
+        redirect_to '/broker'
+    else
+      render '/broker/new'
+    end
+  end
+
+  def authenticate
+    @broker = Admin.find_by_email(params[:broker][:email])
+
+    if @broker && @broker.authenticate(params[:broker][:password])
+      session[:broker] = @broker.id
+      redirect_to '/broker'
+    else
+      @errors = ["invalid username/password"]
+      render 'broker/login'
+    end
+  end
 
   private
+
+  def signup_params
+    params.require(:broker).permit(:name, :signup_key, :email, :password, :password_confirmation)
+  end
 
   def load_broker
     @broker ||= Broker.find(token: params[:token])
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  
+  #===============================================
+  #Broker routes
+  #===============================================
+  get '/broker' => 'broker#show'
 
+  get '/broker/register' => 'broker#new'
+  post '/broker/register' => 'broker#create'
+
+  get '/broker/login' => 'broker#login'
+  post '/broker/login' => 'broker#authenticate'
+  #===============================================
+  #Admin routes
+  #===============================================
   get '/admin' => 'admin#show'
 
   get '/admin/configure' => 'admin#new'
